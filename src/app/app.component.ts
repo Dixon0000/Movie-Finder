252816b8af57e2e4dc62d3b2cc29ccbaaf3b2f48
Dixon0000 <s00227213@atu.ie>
2023-03-14 12:19:58 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IOMDBResponse } from './omdbresponse';
+import { OmdbApiService } from './services/omdb-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movie-finder';
+  title = 'Movie Finder';
+  movieData:IOMDBResponse | undefined;
+  errorMessage:any;
+
+  constructor(private _omdbService:OmdbApiService) {}
+
+  getMovieDetails(movieName:string): void {
+    this._omdbService.getMovieData(movieName).subscribe(
+      movieData => {
+        this.movieData = movieData;
+        if (!this.movieData.Poster) {
+          this.setDefaultPoster();
+        }
+      }
+    );
+  }
+
+  setDefaultPoster() {
+    if (this.movieData) {
+          this.movieData.Poster = 'default-poster.jpg'; // set default image path
+    }
+  }
 }
